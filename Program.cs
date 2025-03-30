44cab214de530f337ccdb56796b62e408730141f
MarcusVHart <haykirengeral@gmail.com>
2025-03-30 13:27:54 -0300

    criação de sistema de Forca, onde o usuário digita uma letra e aparece se existe ou não

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("[___[ F O R C A ]___]");
+
+
+
+Console.WriteLine("Digite a palavra:");
+string palavra = Console.ReadLine()!;
+
+
+
+Console.WriteLine("Digite uma letra:");
+string letra = Console.ReadLine()!
+.Trim()
+.Substring(0, 1)
+.ToLower();
+
+
+
+bool letraExiste = palavra
+.ToLower()
+.Contains(letra);
+
+
+
+Console.WriteLine($"\nA letra \"{letra}\" {(letraExiste ? "existe" : "não existe")} na palavra -> {palavra}");
